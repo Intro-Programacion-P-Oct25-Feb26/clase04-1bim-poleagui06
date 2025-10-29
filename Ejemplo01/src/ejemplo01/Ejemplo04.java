7c6ca243775928f3b0c42576b98bfa08b44a48c3
poleagui06 <jeanpoleagui06@gmail.com>
2025-10-29 12:04:33 -0500

    print - println

--- Ejemplo01/src/ejemplo01/Ejemplo04.java
+++ Ejemplo01/src/ejemplo01/Ejemplo04.java
@@ -10,13 +10,22 @@ package ejemplo01;
  * @author reroes
  */
 public class Ejemplo04 {
+
     public static void main(String[] args) {
         String nombreEstudiante = "René Rolando";
         String apellidoEstudiante = "Elizalde Solano";
         int nacimiento = 1983;
+        // print   => ejecuta en una sola linea (no hace salto de linea.
+        // println => ejecuta dando un salto de linea.
         System.out.print(nombreEstudiante + " ");
         System.out.print(apellidoEstudiante + " ");
         System.out.print(nacimiento + " ");
         System.out.print(nombreEstudiante + " ");
+
+        System.out.println("------------------");
+        System.out.println(nombreEstudiante + " ");
+        System.out.println(apellidoEstudiante + " ");
+        System.out.println(nacimiento + " ");
+        System.out.println(nombreEstudiante + " ");
     }
 }
